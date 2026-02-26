274b5c56e66c6e101f41b5e2aba0bf99b32844af
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-02-26 16:35:22 +0530

    Building markle tree in blockchain

---
+++ Markle_tree.py
@@ -0,0 +1,41 @@
+from typing import List
+import hashlib
+
+class Node:
+    def __init__(self, left, right, value=str, content=None, is_copied=False) -> None:##__init__(automatically called)
+        self.left:Node = left
+        self.right:Node = right
+        self.value = value
+        self.content = content
+        self.is_copied = is_copied
+
+    def hash(val: str) -> str:
+        return hashlib.sha256(val.encode()).hexdigest()
+    
+    def __str__(self) -> str:
+        return self.value
+    
+    def copy(self): 
+        return Node(self.left, self.right, self.value, self.content, True)
+    
+class MerkleTree:
+    def __init__(self, values: List[str]) -> None:
+        self.__buildTree(values)
+
+    def __buildTree(self, values: List[str]) -> None:
+        leaves: List[Node] = [Node(None, None, Node.hash(e), e) for e in values]
+
+        if len(leaves) % 2 == 1: 
+            leaves.append(leaves[-1].copy())
+            self.root = self.__buildTree(leaves)
+    
+    def __buildTree(self, nodes: List[Node]) -> Node:
+        if len(nodes) % 2 == 1:
+            nodes.append(nodes[-1].copy())
+        half: int = len(nodes) // 2
+
+        if len(nodes) == 2:
+            return Node(nodes[0], nodes[1], Node.hash(nodes[0].value + nodes[1].value), nodes[0].content + "+" + nodes[1].content)
+        
+        left: Node = self.__buildTree(nodes[:half])
+        right: Node = self.__buildTree(nodes[half:])
