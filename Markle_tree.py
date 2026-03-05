ccb511dde908664fa34cf45b59434f5ed5acf270
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-03-05 15:37:05 +0530

    update markle tree file fix some errors

--- Markle_tree.py
+++ Markle_tree.py
@@ -1,81 +1,88 @@
+# Python code for implementing Merkle Tree
 from typing import List
 import hashlib
 
 class Node:
-    def __init__(self, left, right, value=str, content=None, is_copied=False) -> None:##__init__(automatically called)
-        self.left:Node = left
-        self.right:Node = right
+    def __init__(self, left, right, value: str, content, is_copied=False) -> None:
+        self.left: Node = left
+        self.right: Node = right
         self.value = value
         self.content = content
         self.is_copied = is_copied
 
     def hash(val: str) -> str:
-        return hashlib.sha256(val.encode()).hexdigest()
-    
-    def __str__(self) -> str:
-        return self.value
-    
-    def copy(self): 
+        return hashlib.sha256(val.encode('utf-8')).hexdigest()
+
+    def __str__(self):
+        return (str(self.value))
+
+    def copy(self):
+        """
+        class copy function
+        """
         return Node(self.left, self.right, self.value, self.content, True)
-    
+
+
 class MerkleTree:
     def __init__(self, values: List[str]) -> None:
         self.__buildTree(values)
 
     def __buildTree(self, values: List[str]) -> None:
-        leaves: List[Node] = [Node(None, None, Node.hash(e), e) for e in values]
 
-        if len(leaves) % 2 == 1: 
+        leaves: List[Node] = [Node(None, None, Node.hash(e), e)
+                              for e in values]
+        if len(leaves) % 2 == 1:
+            # duplicate last elem if odd number of elements
             leaves.append(leaves[-1].copy())
-            self.root = self.__buildTree(leaves)
-    
-    def __buildTree(self, nodes: List[Node]) -> Node:
+        self.root: Node = self.__buildTreeRec(leaves)
+
+    def __buildTreeRec(self, nodes: List[Node]) -> Node:
         if len(nodes) % 2 == 1:
+            # duplicate last elem if odd number of elements
             nodes.append(nodes[-1].copy())
         half: int = len(nodes) // 2
 
         if len(nodes) == 2:
-            return Node(nodes[0], nodes[1], Node.hash(nodes[0].value + nodes[1].value), nodes[0].content + "+" + nodes[1].content)
-        
-        left: Node = self.__buildTree(nodes[:half])
-        right: Node = self.__buildTree(nodes[half:])
+            return Node(nodes[0], nodes[1], Node.hash(nodes[0].value + nodes[1].value), nodes[0].content+"+"+nodes[1].content)
 
+        left: Node = self.__buildTreeRec(nodes[:half])
+        right: Node = self.__buildTreeRec(nodes[half:])
         value: str = Node.hash(left.value + right.value)
-        content: str = f'{left.content} + {right.content}'
-
+        content: str = f'{left.content}+{right.content}'
         return Node(left, right, value, content)
-    
+
     def printTree(self) -> None:
         self.__printTreeRec(self.root)
 
-    def __printTreeRec(self, node: Node, level=0) -> None:
+    def __printTreeRec(self, node: Node) -> None:
         if node != None:
             if node.left != None:
-                print("Left:" +str(node.left))
+                print("Left: "+str(node.left))
                 print("Right: "+str(node.right))
             else:
                 print("Input")
 
             if node.is_copied:
-                print("(padding)")
-            print("value: "+str(node.value))
-            print("content: "+str(node.content))
+                print('(Padding)')
+            print("Value: "+str(node.value))
+            print("Content: "+str(node.content))
             print("")
             self.__printTreeRec(node.left)
             self.__printTreeRec(node.right)
 
-    def getrootHash(self) -> str:
+    def getRootHash(self) -> str:
         return self.root.value
-    
-def mixmarkletree() -> None:
-    elems = ["Medicaps CFI", "A", "computer", "science", "portal", "for", "students"]
 
+def mixmerkletree() -> None:
+    elems = ["Medicaps CFI", "A", "Computer",
+             "Science", "Portal", "For", "Students"]
+    # as there are odd number of inputs, the last input is repeated
     print("Inputs: ")
     print(*elems, sep=" | ")
     print("")
     mtree = MerkleTree(elems)
-    print("Root Hash: "+mtree.getrootHash()+"\n")
+    print("Root Hash: "+mtree.getRootHash()+"\n")
     mtree.printTree()
 
 
-mixmarkletree()
+mixmerkletree()
